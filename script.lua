ddd9fef5ae85de5481ec3132685fd9d08804741d
KnorkzPH <seansanchez0424@gmail.com>
2026-09-04 23:24:33 +0800

    Delete script.lua

--- script.lua
+++
@@ -1,9 +0,0 @@
-local ProtectionConfig = {
-    SecretKey = "4123",
-    HubName = "karinderyascript.txt"
-}
-
-print(ProtectionConfig.HubName .. " Loaded Successfully!")
-
-task.wait(0.7)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Nyorkzgif/Idkwhatisthisfor-/refs/heads/main/Bloxfruit%20script"))()
